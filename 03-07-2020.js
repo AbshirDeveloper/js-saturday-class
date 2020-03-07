f3dbef522cf6b87026435473d93143927f5b128f
unknown <Abshir.uideveloper@gmail.com>
2020-03-07 16:32:31 -0600

    03-07-2020

---
+++ 03-07-2020.js
@@ -0,0 +1,41 @@
+// else if practice
+
+// else if practice
+
+/* 
+
+A, B, C, D, E, F
+
+A= Excelent
+B= Very good
+C= Good
+D= Poor
+E= Very poor
+F= Failed
+
+*/
+
+const grades ={
+ A: "Excelent",
+B: 'Very good',
+C: 'Good',
+D:' Poor',
+E: 'Very poor',
+F: 'Failed',
+
+};
+
+const grade = "poor";
+
+if(grade==="excelent"){
+
+    console.log("A");
+} else if (grade==="poor"){
+    console.log("D");
+}else if (grade==="failed"){
+    console.log ()
+}
+
+
+
+
