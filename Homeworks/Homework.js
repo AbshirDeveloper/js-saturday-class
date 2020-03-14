ff4d368319238cc855fedc0a4e4e421a430c7bb8
unknown <Abshir.uideveloper@gmail.com>
2020-03-14 15:11:19 -0500

    homework1

---
+++ Homeworks/Homework.js
@@ -0,0 +1,23 @@
+
+var sport = "football";
+
+const age = prompt('what is your age?');
+
+if(age >  20 ){
+document.write("sorry mate, you are too old!");
+}
+else { 
+    if (age < 20){
+    var newAnswer= prompt("which sport do you like mate?")
+}
+if (newAnswer===sport){
+    document.write("you are a hero");
+}else {
+    document.write("you are foolish");
+}
+}
+
+        
+        
+        
+            
